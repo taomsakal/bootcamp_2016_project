992429cc0621e4f099c4b6035b16c392838633de
snowykyoto <imackey1415@gmail.com>
2016-09-08 20:38:26 -0700

    finally close

--- Ndiffusion.py
+++ Ndiffusion.py
@@ -11,30 +11,27 @@ import matplotlib
 import scipy.io as sio
 
 
-nodes = 20
-G = nx.watts_strogatz_graph(nodes,3,0.5)
+nodes = 15
+sick = 4
+G = nx.erdos_renyi_graph(nodes,0.4)
 A = nx.adjacency_matrix(G)
 
-#print(A.todense())
+print(A.todense())
 
 beta=1
 gamma=2
 
 n=A.shape[1]
 
-#generate initial health of population (0 = uninfected)
-I_init=np.asarray([0]*nodes)
-I_init[0]=1
-
-S_init = 1 - I_init
-
 #make sure length of I_init is equal to number of nodes
-I_init = np.random.randint(2,size=nodes)
+#I_init = np.random.randint(2,size=nodes)
+
+I_init = np.asarray([1]*(sick)+[0]*(nodes-sick))
+
 S_init = 1-I_init
 
-
 #T = iterations of propagation to run
-T=100
+T=10
 
 #dt = size of time interval to simulate
 dt=1
@@ -52,14 +49,19 @@ I[:, 0] = I_init
 S[:, 0] = S_init
 
 #the ODE
-for t in range(0, T-2):
-    for i in range(0, n-1):
-    	sum1=0;
-        for j in range(0, n-1):
-        	sum1 = sum1 + A[i, j]*I[j,t];
+for t in range(0, T-1):
+	for i in range(0, n-1):
+    		sum1=0;
+        	for j in range(0, n-1):
+       			sum1 = sum1 + A[i, j]*I[j,t];
 
-	I[i,t+1] = I[i,t] + dt*beta*S[i,t]*sum1;
-    	S[i,t+1] = 1 - I[i,t+1];
+		I[i,t+1] = max(0,min(1,I[i,t] + dt*beta*S[i,t]*sum1))
+		S[i,t+1] = max(0,min(1,S[i,t] - dt*beta*S[i,t]*sum1))
+
+print I
+
+#import sys
+#sys.exit()
 
 S_w= np.dot(vmax,S)
 I_w= np.dot(vmax,I)
@@ -67,11 +69,11 @@ I_w= np.dot(vmax,I)
 pl.figure(1)
 
 #Ploting
-pl.plot(S, '-bs', label='Susceptibles')
-#pl.plot(S_w, '-bs', label='Susceptibles')
+#pl.plot(S, '-bs', label='Susceptibles')
+pl.plot(S_w, '-bs', label='Susceptibles')
 
-pl.plot(I, '-ro', label='Infectious')
-#pl.plot(I_w, '-ro', label='Infectious')
+#pl.plot(I, '-ro', label='Infectious')
+pl.plot(I_w, '-ro', label='Infectious')
 
 #pl.legend(loc=0)
 pl.title('SI epidemic without births or deaths')

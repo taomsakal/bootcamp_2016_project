92cbb3a881474fce295e094c398c9a39b9547e00
snowykyoto <imackey1415@gmail.com>
2016-09-08 18:36:14 -0700

    edited Ndiff.py

--- Ndiffusion.py
+++ Ndiffusion.py
@@ -4,32 +4,34 @@ import numpy as np
 import pylab as pl
 import networkx as nx
 from numpy import linalg as la
-
+from random import random 
 import matplotlib
 
 import scipy.io as sio
 
 
-nodes = 13
-G = nx.watts_strogatz_graph(nodes,3,0.4)
+nodes = 20
+G = nx.watts_strogatz_graph(nodes,3,0.5)
 A = nx.adjacency_matrix(G)
 
-print(A.todense())
+#print(A.todense())
 
 beta=1
 gamma=2
 
 n=A.shape[1]
 
-#make sure length of I_init is equal to number of nodes
-I_init = [random.randint(0,1) for _ in range(nodes)]
-I_init=np.asarray(I_init)
+#generate initial health of population (0 = uninfected)
+I_init=np.asarray([0]*nodes)
+I_init[0]=1
 
-S_init = [1-i for i in I_init]
-S_init=np.asarray(S_init)
+S_init = 1 - I_init
 
 
+#T = iterations of propagation to run
 T=100
+
+#dt = size of time interval to simulate
 dt=1
 
 #eigenvalues, eigenvectors = la.eig(np.asarray(A))
@@ -41,43 +43,30 @@ vmax = np.asarray([1]*nodes)
 I=np.zeros((n, T))
 S=np.zeros((n, T))
 
-
 I[:, 0] = I_init
 S[:, 0] = S_init
-
-sum=0;
-
-print "I"
-print I
-
-print "S"
-print S
-
 
 #the ODE
 for t in range(0, T-2):
     for i in range(0, n-1):
-    	sum=0;
+    	sum1=0;
         for j in range(0, n-1):
-        	sum=sum + A[i, j]*I[j,t];
-    	
-	I[i,t+dt] = I[i,t] + dt*beta*S[i,t]*sum;
-    	S[i,t+dt] = S[i,t] - dt*beta*S[i,t]*sum;
+        	sum1 = sum1 + A[i, j]*I[j,t];
 
-S_w= np.dot(np.transpose(vmax),S)
+	I[i,t+1] = I[i,t] + dt*beta*S[i,t]*sum1;
+    	S[i,t+1] = 1 - I[i,t+1];
+
+S_w= np.dot(vmax,S)
 I_w= np.dot(vmax,I)
 
-print I
-print S
-#print I_w
-
 pl.figure(1)
-#Ploting
-#pl.plot(S, '-bs', label='Susceptibles')
-pl.plot(S_w, '-bs', label='Susceptibles')
 
-#pl.plot(I, '-ro', label='Infectious')
-pl.plot(I_w, '-ro', label='Infectious')
+#Ploting
+pl.plot(S, '-bs', label='Susceptibles')
+#pl.plot(S_w, '-bs', label='Susceptibles')
+
+pl.plot(I, '-ro', label='Infectious')
+#pl.plot(I_w, '-ro', label='Infectious')
 
 #pl.legend(loc=0)
 pl.title('SI epidemic without births or deaths')
